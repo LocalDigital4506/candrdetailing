25c7251e94a31396835301f12aa70da08699be59
LocalDigital4506 <LocalDigital4506@gmail.com>
2025-06-08 23:24:04 -0500

    Add files via upload

---
+++ vite.config.cjs
@@ -0,0 +1,18 @@
+const { defineConfig } = require("vite");
+const react = require("@vitejs/plugin-react");
+const path = require("path");
+
+module.exports = defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./client/src"),
+      "@assets": path.resolve(__dirname, "./attached_assets"),
+    },
+  },
+  root: "./client",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
+});
